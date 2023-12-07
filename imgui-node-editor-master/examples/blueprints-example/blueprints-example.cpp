f7317c2633487ab560c9f5c3ffd1ee7d2611a12d
Aster Nie <asterwnie@gmail.com>
2023-12-06 19:27:04 -0500

    Adding JsonCpp library for json i/o (https://github.com/open-source-parsers/jsoncpp)

--- imgui-node-editor-master/examples/blueprints-example/blueprints-example.cpp
+++ imgui-node-editor-master/examples/blueprints-example/blueprints-example.cpp
@@ -12,6 +12,9 @@
 #include <algorithm>
 #include <utility>
 
+#include <json/value.h>
+#include <fstream>
+
 
 static inline ImRect ImGui_GetItemRect()
 {
@@ -629,6 +632,23 @@ struct Example :
     void LoadBlendTreeFromJSON()
     {
         // load json file
+        // example from https://stackoverflow.com/questions/32205981/reading-json-files-in-c
+        /*
+        std::ifstream people_file("people.json", std::ifstream::binary);
+        Json::Value people;
+        people_file >> people;
+
+        cout<<people; //This will print the entire json object.
+
+        //The following lines will let you access the indexed objects.
+        cout<<people["Anna"]; //Prints the value for "Anna"
+        cout<<people["ben"]; //Prints the value for "Ben"
+        cout<<people["Anna"]["profession"]; //Prints the value corresponding to "profession" in the json for "Anna"
+
+        cout<<people["profession"]; //NULL! There is no element with key "profession". Hence a new empty element will be created.
+        */
+
+
 
         // display depending on node type
 
